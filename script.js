3c132805dee757df0c3c81ecc95fbe3b9add01dd
lomfley <lomfley@gmail.com>
2025-08-04 18:50:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let score = 0;
+const scoreDisplay = document.getElementById('score');
+const button = document.getElementById('clickBtn');
 
+button.addEventListener('click', () => {
+  score++;
+  scoreDisplay.textContent = score;
+});
